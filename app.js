7ee7fb12b61d5802a4ec456562f0547d6f0a1fe7
STACSAH <89057127+STACSAH@users.noreply.github.com>
2023-05-11 16:31:02 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,164 @@
+//jshint esversion:6
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://Cluster67555:WmB5cntke0pf@cluster67555.pzybjxz.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+// const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});//localhost
+mongoose.connect("mongodb+srv://Cluster67555:WmB5cntke0pf@cluster67555.pzybjxz.mongodb.net/todolistDB")
+
+const itemsSchema = {
+  name: String
+}
+const Item = mongoose.model("Item",itemsSchema); // model is capitalise
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1,item2,item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+const List = mongoose.model("List",listSchema);
+
+// Item.deleteOne({name:"Hit the + button to add a new item"}).then(()=>{
+//   console.log("Deleted!");
+// }).catch((err)=>{
+//   console.log(err);
+// })
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.get("/", function(req, res) {
+  Item.find().then((items)=>{
+    if (items.length === 0){
+      Item.insertMany(defaultItems).then(()=>{
+        console.log('Inserted!');
+      }).catch(function(err){
+        console.log(err);
+      });
+      res.redirect("/");
+    }else{
+    res.render("list", {listTitle: "Today", newListItems: items});
+  }
+  })
+});
+
+app.get("/:pageName",(req,res)=>{
+  const pageName = _.capitalize(req.params.pageName); // after the /<pageName>
+  
+  List.findOne({name:pageName}).then(function(foundList){
+    if (!foundList){
+      const list = new List({
+        name: pageName,
+        items: defaultItems
+      });
+      list.save();
+      res.redirect("/"+pageName);
+  }else{
+    res.render("list",{listTitle:foundList.name,newListItems:foundList.items})
+  }
+  }).catch(function(err){
+    console.log(err);
+  });
+
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const toAdd = new Item({name:itemName});
+  
+  if (listName === "Today"){
+  toAdd.save();
+  res.redirect("/");
+}else{
+  List.findOne({name:listName}).then(function(foundList){
+    foundList.items.push(toAdd);
+    foundList.save();
+    res.redirect("/"+listName);
+  })
+}
+
+ 
+});
+
+app.post("/delete",function(req,res){
+  const checkedItemId = req.body.checkbox;
+  const listTitle = req.body.listTitle;
+  if (listTitle === "Today"){
+  Item.findByIdAndRemove(checkedItemId).then(()=>{
+  console.log("Deleted!")
+  }).catch(function(err){
+    console.log(err);
+  });
+  
+  res.redirect("/");
+}else{
+  List.findOneAndUpdate({name:listTitle},{$pull:{items:{_id:checkedItemId}}}).then((foundList)=>{
+    res.redirect("/"+listTitle);
+  }).catch(function(err){
+    console.log(err);
+  })
+}
+
+})
+
+
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
